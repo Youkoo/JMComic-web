8de44345a5ae4ef3550995c8abdb8b031b00da1b
LingLambda <abc1514671906@163.com>
2025-03-24 18:16:04 +0800

    添加 is_path 配置

--- main.py
+++ main.py
@@ -1,45 +1,26 @@
-from jmcomic import create_option_by_file, download_album
-from pathlib import Path
-
-
-def get_album_pdf_path(jm_album_id):
-    # 读取配置文件
-    opt = create_option_by_file('./option.yml')
-
-    # 下载本子（只下载单文件，不需要 unpack tuple）
-    album, _ = download_album(jm_album_id, option=opt)
-
-    pdf_dir = opt.plugins.after_album[0].kwargs.get('pdf_dir', './')
-
-    # 将 pdf_dir 转换为 Path 对象
-    pdf_dir_path = Path(pdf_dir).resolve()  # 使用 resolve() 获取绝对路径
-
-    # 检查目录是否存在
-    if not pdf_dir_path.exists():
-        return None
-    else:
-        # 返回完整的文件路径
-        return str(pdf_dir_path / f"{album.title}.pdf")
-
-
 from flask import Flask
 from waitress import serve
+
+from album_service import get_album_pdf_path
 
 app = Flask(__name__)
 
 
-@app.route('/get_pdf_path/<jm_album_id>', methods=['GET'])
-def get_pdf_path(jm_album_id):
-    path = get_album_pdf_path(jm_album_id)
-    if path is None:
+@app.route('/get_pdf_path/<jm_album_id>/<is_path>', methods=['GET'])
+def get_pdf_path(jm_album_id,is_path):
+    is_path = is_path.lower() in ['true', '1', 'yes']
+    data,name = get_album_pdf_path(jm_album_id,is_path)
+    if data is None:
         return {
             "success": False,
+            "name": name,
             "data": ""
         }
     else:
         return {
             "success": True,
-            "data": path
+            "name": name,
+            "data": data
         }
 
 
